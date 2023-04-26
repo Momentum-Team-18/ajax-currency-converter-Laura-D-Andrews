a96aafc3955fa1c65c37bb6f1a157fa975ae8a32
Laura-D-Andrews <ldandrews88@gmail.com>
2023-04-26 18:10:19 -0400

    started grid and added JS elements

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
-// fetch request: `https://openexchangerates.org/api/latest.json?app_id=APP_ID&base=BASE_CURRENCY`
-// `BASE_CURRENCY` is a placeholder for the base currency code
-// `APP_ID` is placeholder for access key - d176522a23eb485885d8f97ffb0cf101
 
-// User flow
-// user chooses target currency from dropdown
-// user inputs $ amount to convert to target currency (via text)
-// user clicks convert button
+console.log(currencies)
 
-// JS/API flow
-    // "chooses target currency" - reads target currency
-    // event listener - update user interface
-    // "clicks to change $ amount" - reads the new dollar amount
-    // event listener - update user interface
-    // "clicks convert button" - read the base and target currencies from dropdown
-    // makes a fetch request? 
-    // wait on response
-    // use response to update user interface to show target currency amount
-    
+let containerBox = document.querySelector("#containerBox");
+console.log(containerBox)
 
-// need to incorporate 
-    // dropdown list of currencies - use currencies.js file in this repo
+// header
+let headerBox = document.createElement("div")
+headerBox.innerText = "Currency Calculator"
+containerBox.appendChild(headerBox)
+headerBox.classList.add("header")
+console.log(headerBox)
 
-// event listeners
-    // convert button "click"
-    // choose currency - update user interface
-    // changes $ amount - update user interface
+// base currency
+let baseCurrencyBox = document.createElement("div");
+baseCurrencyBox.innerText = "USD";
+containerBox.appendChild(baseCurrencyBox);
+baseCurrencyBox.classList.add("baseCurrency");
+console.log(baseCurrencyBox)
+
+// target currency
+let targetCurrencyBox = document.createElement("div");
+targetCurrencyBox.innerText = "EUR";
+containerBox.appendChild(targetCurrencyBox);
+targetCurrencyBox.classList.add("targetCurrency");
+console.log(targetCurrencyBox)
+
+// input amount
+let inputAmountBox = document.createElement('div')
+inputAmountBox.innerText = "1,000"
+containerBox.appendChild(inputAmountBox)
+inputAmountBox.classList.add("inputAmount")
+console.log(inputAmountBox)
+
+// output amount
+let outputAmountBox = document.createElement('div')
+outputAmountBox.innerText = "1,000"
+containerBox.appendChild(outputAmountBox)
+outputAmountBox.classList.add("outputAmount")
+console.log(outputAmountBox)
+
+// exchange equation box
+
+let exchangeEquationBox = document.createElement('div')
+exchangeEquationBox.innerText = "1 EUR = 1 USD"
+containerBox.appendChild(exchangeEquationBox)
+exchangeEquationBox.classList.add("exchangeEquation")
+console.log(exchangeEquationBox)
