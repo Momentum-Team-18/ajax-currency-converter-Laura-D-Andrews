6c9560e66b6922cf925b267b9ea3e837b135dfa5
Laura-D-Andrews <ldandrews88@gmail.com>
2023-04-27 21:17:45 -0400

    added some loops

--- main.js
+++ main.js
@@ -16,28 +16,41 @@ fetch(ratesUrl, {
     console.log(Array.isArray(parsedResponse));
     const makeAnArray = parsedResponse.rates;
 
+    // country codes - needs to be a dropdown box
     let countryCodes = Object.keys(makeAnArray);
-    console.log(countryCodes);
-    countryCodes = document.createElement("div");
-    countryCodes.innerText = "placeholder"
-    containerBox.appendChild(countryCodes);
-    countryCodes.classList.add("countryCodes");
-    console.log(countryCodes);
+    for (code of countryCodes) {
+      countryCodes = document.createElement("div");
+      countryCodes.innerText = `${code}`;
+      containerBox.appendChild(countryCodes);
+      countryCodes.classList.add("countryCodes");
+      console.log(countryCodes);
+    }
+
+    // country exchange rates
+    let countryRates = Object.values(makeAnArray);
+    for (rate of countryRates) {
+      countryRates = document.createElement("div");
+      countryRates.innerText = `${rate}`;
+      containerBox.appendChild(countryRates);
+      countryRates.classList.add("countryRates");
+      console.log(countryRates);
+    }
+
+    // $ input box
+    let inputAmountBox = document.createElement("textarea");
+    inputAmountBox.innerText = "";
+    containerBox.appendChild(inputAmountBox);
+    inputAmountBox.classList.add("inputAmount");
+    console.log(inputAmountBox);
+
+    // base currency box
+    let baseCurrency = document.createElement("div");
+    baseCurrency.innerText = "USD";
+    containerBox.appendChild(baseCurrency);
+    baseCurrency.classList.add("baseCurrency");
+    console.log(baseCurrency);
+
+    // let targetFunction = (countryCodes, countryRates) =>
 
     
-let inputAmountBox = document.createElement("textarea")
-inputAmountBox.innerText = "1,000"
-containerBox.appendChild(inputAmountBox)
-inputAmountBox.classList.add("inputAmount")
-console.log(inputAmountBox)
-
-    let countryRates = Object.values(makeAnArray);
-    console.log(countryRates);
-    countryRates = document.createElement("div");
-    countryRates.value = "placeholder"
-    containerBox.appendChild(countryRates);
-    countryRates.classList.add("countryRates");
-    console.log(countryRates);
-
-
   });
