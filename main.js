160d9b3cf5e70ec4dd6918bfc0cd482ee1f75c45
Laura-D-Andrews <ldandrews88@gmail.com>
2023-05-01 12:53:32 -0400

    calculate button working

--- main.js
+++ main.js
@@ -16,15 +16,24 @@ fetch(ratesUrl, {
     console.log(Array.isArray(parsedResponse));
     const makeAnArray = parsedResponse.rates;
 
-    // country codes - needs to be a dropdown box
+    //codes and codesBox - not functioning as dropdown yet
+    let codesBox = document.createElement("div");
+    codesBox.innerText = "";
+    containerBox.appendChild(codesBox);
+    codesBox.classList.add("countryCodes");
+    console.log(codesBox);
+
     let countryCodes = Object.keys(makeAnArray);
     for (code of countryCodes) {
       countryCodes = document.createElement("div");
       countryCodes.innerText = `${code}`;
-      containerBox.appendChild(countryCodes);
+      codesBox.appendChild(countryCodes);
       countryCodes.classList.add("countryCodes");
       console.log(countryCodes);
     }
+
+
+
 
     // country exchange rates
     let countryRates = Object.values(makeAnArray);
@@ -35,22 +44,60 @@ fetch(ratesUrl, {
       countryRates.classList.add("countryRates");
       console.log(countryRates);
     }
-
     // $ input box
     let inputAmountBox = document.createElement("textarea");
-    inputAmountBox.innerText = "";
     containerBox.appendChild(inputAmountBox);
     inputAmountBox.classList.add("inputAmount");
     console.log(inputAmountBox);
 
-    // base currency box
-    let baseCurrency = document.createElement("div");
-    baseCurrency.innerText = "USD";
-    containerBox.appendChild(baseCurrency);
-    baseCurrency.classList.add("baseCurrency");
-    console.log(baseCurrency);
+    // exchange box - when click calculate button exchange amount shows here
+    let exchangeBox = document.createElement("div");
+    exchangeBox.innerText = ''
+    containerBox.appendChild(exchangeBox);
+    console.log(exchangeBox);
+    exchangeBox.classList.add("exchangeBox");
 
-    // let targetFunction = (countryCodes, countryRates) =>
 
-    
+
+    // creates calculate button
+    let calculateButton = document.createElement("div");
+    calculateButton.innerText = "calculate";
+    containerBox.appendChild(calculateButton);
+    calculateButton.classList.add("calculate")
+   
+
+    // calculate button listener - click calculate button and currency conversion appears in exchange boc
+    calculateButton.addEventListener("click", (event) => { 
+      console.log(event.target.innerText);
+      let answer = inputAmountBox.value * `${rate}`;
+      console.log(answer);
+      exchangeBox.innerText = answer
+    });
   });
+
+
+
+
+let baseCurrency = document.createElement("div");
+baseCurrency.innerText = "USD";
+containerBox.appendChild(baseCurrency);
+baseCurrency.classList.add("baseCurrency");
+console.log(baseCurrency);
+
+let headerBox = document.createElement("div");
+headerBox.innerText = "Currency Converter";
+containerBox.appendChild(headerBox);
+headerBox.classList.add("header");
+console.log(headerBox);
+
+let fromBox = document.createElement("div");
+fromBox.innerText = "From";
+containerBox.appendChild(fromBox);
+fromBox.classList.add("from");
+console.log(fromBox);
+
+let toBox = document.createElement("div");
+toBox.innerText = "To";
+containerBox.appendChild(toBox);
+toBox.classList.add("to");
+console.log(toBox);
