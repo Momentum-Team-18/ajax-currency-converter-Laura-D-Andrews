7285bca549c251cb7b815d74c250b0ad29640294
Laura-D-Andrews <ldandrews88@gmail.com>
2023-05-02 18:22:04 -0400

    updates

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let containerBox = document.querySelector("#containerBox");
-let countryCodesBox = document.querySelector("#countryCodesBox")
-let inputAmountBox = document.querySelector("#inputAmountBox")
-let form = document.querySelector("#form")
-let convertButton = document.querySelector('.convertButton')
-let startOverButton = document.querySelector('.startOverButton')
+let countryCodesBox = document.querySelector("#countryCodesBox"); // blue background
+let inputAmountBox = document.querySelector("#inputAmountBox");
+let form = document.querySelector("#form");
+let convertButton = document.querySelector(".convertButton");
+let startOverButton = document.querySelector(".startOverButton");
 let ratesUrl =
   "https://openexchangerates.org/api/latest.json?app_id=d176522a23eb485885d8f97ffb0cf101";
 
@@ -16,7 +16,6 @@ fetch(ratesUrl, {
     return response.json();
   })
   .then((parsedResponse) => {
-    console.log(parsedResponse.rates);
     console.log(Array.isArray(parsedResponse));
     const makeAnArray = parsedResponse.rates;
 
@@ -26,50 +25,50 @@ fetch(ratesUrl, {
       countryCodes.innerText = `${code}`;
       countryCodesBox.appendChild(countryCodes);
       countryCodes.classList.add("countryCodes");
-      console.log(countryCodes);
+      // appended in blue countryCodesBox
     }
 
     let countryRates = Object.values(makeAnArray);
     for (rate of countryRates) {
-    console.log(Array.isArray(countryRates));
+      console.log(Array.isArray(countryRates));
     }
 
-    let codeRateArray = Object.entries(makeAnArray);
-    console.log(codeRateArray);
-    console.log(Array.isArray(codeRateArray));
+    // let codeRateArray = Object.entries(makeAnArray);
 
     let outputBox = document.createElement("div");
-    outputBox.innerText = `${rate}`;
     containerBox.appendChild(outputBox);
-    console.log(outputBox);
-    outputBox.classList.add("ratesBox");
+    outputBox.classList.add("outputBox");
+    // brown background
 
     let exchangeRateBox = document.createElement("div");
     exchangeRateBox.innerText = "0.00";
     containerBox.appendChild(exchangeRateBox);
-    console.log(exchangeRateBox);
-    exchangeRateBox.classList.add("exchangeBox");
+    exchangeRateBox.classList.add("exchangeRateBox");
+    //yellow background
+
 
     // event listeners
     countryCodesBox.addEventListener("click", (event) => {
       console.log(event.target.innerText);
-      let exchangeRate = "? amount";
-      console.log(exchangeRate);
-      exchangeRateBox.innerText = `${rate}` + `${countryCodes.innerText}` //how to make a space
+      let exchangeRateText = `${rate}` + `${countryCodes.innerText}`;
+      exchangeRateBox.innerText = exchangeRateText;
+      /* goal - when choose country code (blue) from dropdown list, exchange rate 
+      and the same country code populate in exchange rate box (yellow)
+      tried adding event listener to countryCodes, but doesn't work in browser
+      and nothing shows on console*/
     });
 
     convertButton.addEventListener("click", (event) => {
       console.log(event.target.innerText);
       let answer = inputAmountBox.value * `${rate}`;
-      console.log(answer);
-      outputBox.innerText = answer 
+      outputBox.innerText = answer;
     });
 
-    startOverButton.addEventListener('click', (event) => {
-      event.preventDefault()
-      exchangeRateBox.replaceChildren()
-      outputBox.replaceChildren()
-      form.reset()      
-    })
-    
+    startOverButton.addEventListener("click", (event) => {
+      console.log(event.target.innerText)
+      event.preventDefault();
+      exchangeRateBox.replaceChildren();
+      outputBox.replaceChildren();
+      form.reset();
+    });
   });
