f5d4d074381fd81f29126d90b8016bb3337c964a
Laura-D-Andrews <ldandrews88@gmail.com>
2023-05-03 16:43:54 -0400

    it works yay

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let containerBox = document.querySelector("#containerBox");
-let countryCodesBox = document.querySelector("#countryCodesBox"); // blue background
+let countryCodesBox = document.querySelector("#countryCodesBox"); 
 let inputAmountBox = document.querySelector("#inputAmountBox");
 let form = document.querySelector("#form");
 let convertButton = document.querySelector("#convertButton");
@@ -8,7 +8,6 @@ let ratesUrl =
   "https://openexchangerates.org/api/latest.json?app_id=d176522a23eb485885d8f97ffb0cf101";
 
 fetch(ratesUrl, {
-  // mode: 'no-cors',
   method: "GET",
   headers: { "Content-Type": "application.json" },
 })
@@ -17,67 +16,49 @@ fetch(ratesUrl, {
   })
   .then((parsedResponse) => {
     const makeAnArray = parsedResponse.rates;
-    let exchangeRate
-
+   
     let countryCodes = Object.keys(makeAnArray);
     for (let code of countryCodes) {
       countryCodes = document.createElement("option");
       countryCodes.innerText = `${code}`;
       countryCodesBox.appendChild(countryCodes);
       countryCodes.classList.add("countryCodes");
-      // appended in blue countryCodesBox
     }
-    
+
     let countryRates = Object.values(makeAnArray);
     for (rate of countryRates) {
     }
-    
-    // let converterArray = Object.entries(makeAnArray);
-    
-    let outputBox = document.createElement("div");
-    containerBox.appendChild(outputBox);
-    outputBox.classList.add("outputBox");
-    // brown background
-    
-    let exchangeRateBox = document.createElement("div");
-    containerBox.appendChild(exchangeRateBox);
-    exchangeRateBox.classList.add("exchangeRateBox");
-    //yellow background
-    
-    
-    // event listeners
-  //   countryCodesBox.addEventListener("change", (event) => {
-  //   console.log(event.target.innerText);  
-  //      let exchangeRateText = `${rate}` + `${countryCodes.innerText}`;
-  //     console.log(countryCodes.innerText)
-  //     exchangeRateBox.textContent = exchangeRateText;
-  // });
 
-  countryCodesBox.addEventListener("change", (event) => {
-  const currencyCode = event.target.value
-  const exchangeRate = makeAnArray[currencyCode]
-  console.log(exchangeRate)
-  const exchangeRateText = `${currencyCode}  ${exchangeRate}`
-  exchangeRateBox.innerText = exchangeRateText
-  })
-      
+    let userOutputBox = document.createElement("div");
+    containerBox.appendChild(userOutputBox);
+    userOutputBox.classList.add("outputBox");
+    
+
+    let usdToExchangeRateBox = document.createElement("div");
+    containerBox.appendChild(usdToExchangeRateBox);
+    usdToExchangeRateBox.classList.add("exchangeRateBox");
+    
+
+    countryCodesBox.addEventListener("change", (event) => {
+      let currencyCode = event.target.value;
+      let exchangeRate = makeAnArray[currencyCode];
+      let usdToExchangeRateText = `${exchangeRate} ${currencyCode}`;
+      usdToExchangeRateBox.innerText = usdToExchangeRateText;
+    });
+
     convertButton.addEventListener("click", (event) => {
-     const inputAmount = inputAmountBox.value
-     console.log(inputAmount)
-     const newAmount = inputAmount * exchangeRate
-     console.log(exchangeRate)
-     console.log(newAmount)
-    outputBox.innerText = newAmount;
-
+      let userInputAmount = inputAmountBox.value;
+      let userCurrencyCode = countryCodesBox.value;
+      let userExchangeRate = makeAnArray[userCurrencyCode];
+      let userMoneyAmount = userExchangeRate * userInputAmount;
+      userOutputBox.innerText = userMoneyAmount;
     });
 
     startOverButton.addEventListener("click", (event) => {
-      console.log(event.target.innerText)
+      console.log(event.target.innerText);
       event.preventDefault();
-      exchangeRateBox.replaceChildren();
-      outputBox.replaceChildren();
+      usdToExchangeRateBox.replaceChildren();
+      userOutputBox.replaceChildren();
       form.reset();
     });
-  
   });
-
